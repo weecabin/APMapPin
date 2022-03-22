57bf506f0ab3781193da8012c35e192562704b52
weecabin <weecabin-github@yahoo.com>
2022-03-22 08:52:55 -0700

    Initial Commit

---
+++ APMapPin/APMapPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  APMapPinApp.swift
+//  APMapPin
+//
+//  Created by Gary Hamann on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct APMapPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
